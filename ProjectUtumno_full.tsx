8a6f05a0fb9369e32af91158d1ecbd1c38f42f14
yveso <yveso@users.noreply.github.com>
2021-02-16 20:45:06 +0100

    ...

--- ProjectUtumno_full.tsx
+++ ProjectUtumno_full.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="ProjectUtumno_full" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
  <image source="tiles/ProjectUtumno_full.png" width="2048" height="3040"/>
+ <tile id="202">
+  <animation>
+   <frame tileid="202" duration="300"/>
+   <frame tileid="203" duration="300"/>
+   <frame tileid="204" duration="300"/>
+   <frame tileid="205" duration="300"/>
+   <frame tileid="206" duration="300"/>
+   <frame tileid="210" duration="300"/>
+  </animation>
+ </tile>
 </tileset>
